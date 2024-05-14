fa94577bcc264688ac6b29df7cd926dd9e4291c7
kdong <khoidong.ca@gmail.com>
2024-05-14 13:14:51 -0500

    added context

--- src/App.js
+++ src/App.js
@@ -1,62 +1,20 @@
-import { useState } from "react";
-import axios from "axios";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import BooksContext from "./context/books";
 
 function App() {
-    const [books, setBooks] = useState([]);
+    const { fetchBooks } = useContext(BooksContext);
 
-    const fetchBooks = async () => {
-        const response = await axios.get("http://localhost:3001/books");
-
-        setBooks(response.data);
-    };
     useEffect(() => {
         fetchBooks();
     }, []);
 
-    const editBookById = async (id, newTitle) => {
-        const response = await axios.put(`http://localhost:3001/books/${id}`, {
-            title: newTitle,
-        });
-
-        const updateBooks = books.map((book) => {
-            if (book.id === id) {
-                return { ...book, ...response.data };
-            }
-
-            return book;
-        });
-
-        setBooks(updateBooks);
-    };
-
-    const deleteBookById = async (id) => {
-        const response = await axios.delete(`http://localhost:3001/books/${id}`);
-
-        const updateBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-
-        setBooks(updateBooks);
-    };
-
-    const createBook = async (title) => {
-        const response = await axios.post("http://localhost:3001/books", {
-            title,
-        });
-
-        const updateBooks = [...books, response.data];
-
-        setBooks(updateBooks);
-    };
-
     return (
         <div className="app">
             <h1>Reading List</h1>
-            <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
-            <BookCreate onCreate={createBook} />
+            <BookList />
+            <BookCreate />
         </div>
     );
 }
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import BookEdit from "./BookEdit";
+import useBooksContext from "../hooks/use-books-context";
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
     const [showEdit, setShowEdit] = useState(false);
+    const { deleteBookById } = useBooksContext();
 
     const handleDeleteClick = () => {
-        onDelete(book.id);
+        deleteBookById(book.id);
     };
 
     const handleEditClick = () => {
         setShowEdit(!showEdit);
     };
 
-    const handleSubmit = (id, newTitle) => {
+    const handleSubmit = () => {
         setShowEdit(false);
-        onEdit(id, newTitle);
     };
 
     let content = <h3>{book.title}</h3>;
@@ -24,7 +25,7 @@ function BookShow({ book, onDelete, onEdit }) {
 
     return (
         <div className="book-show">
-            <img alt="book" src={`https://picsum.photos/seed/${book.id}/300/200`} />
+            <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
             <div>{content}</div>
             <div className="actions">
                 <button className="edit" onClick={handleEditClick}>
